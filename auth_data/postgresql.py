42cb9b53880fd32dc483111e7192b4966411db53
analyticston <analytics.ton@gmail.com>
2024-09-22 11:40:41 +0300

    Add files via upload

---
+++ auth_data/postgresql.py
@@ -0,0 +1,13 @@
+postgresql_data = {
+    'connect': {
+        'host':     'YOUR_POSTGRESQL_HOST',
+        'port':     'YOUR_POSTGRESQL_PORT',
+        'dbname':   'YOUR_POSTGRESQL_DBNAME',
+        'user':     'YOUR_POSTGRESQL_USERNAME',
+        'password': 'YOUR_POSTGRESQL_PASSWORD'
+    },
+    'table': { # Where will the data be saved?
+        'schema':   'YOUR_POSTGRESQL_SCHEMA',
+        'table':    'YOUR_POSTGRESQL_TABLE'
+    }
+}
---
+++ auth_data/tg_alert_bot.py
@@ -0,0 +1,4 @@
+is_tg_alert_enabled = False # True - logs will be sent to the bot, False - python print()
+tg_access_token = 'YOUR_TG_ACCESS_TOKEN'
+tg_chat_id = 'YOUR_TG_CHAT_ID'
+tg_support_account = 'anmendel' #This is my telegram username. This is an example, replace it with your own.
